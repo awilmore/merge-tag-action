84d7ad35f75f81c37530f8f9531e2d030e21e9d9
Adam Wilmore <adam@mx51.io>
2020-07-05 11:49:37 +1000

    Added 'await' to createTag (no idea what I'm doing

--- index.js
+++ index.js
@@ -17,32 +17,35 @@ async function run() {
     // if just merged, tag and release
     if (github.context.payload.action === "closed" && github.context.payload.pull_request.merged === true) {
       // Create annotated tag
-      core.debug(`Creating annotated tag: ${nextVersion} ...`)
-      tagResponse = client.git.createTag({
+      core.debug(`[core.debug] Creating annotated tag: ${nextVersion} ...`)      // Trying different log methods
+      console.log(`[console.log] Creating annotated tag: ${nextVersion} ...`)    // Trying different log methods
+      console.info(`[console.info] Creating annotated tag: ${nextVersion} ...`)  // Trying different log methods
+      tagResponse = await client.git.createTag({
         owner: ref.owner,
         repo: ref.repo,
         tag: nextVersion,
         message: "[RELMGMT: Tagged " + nextVersion + "]",
         object: process.env.GITHUB_SHA,
-        type: "commit",
+        type: "commit"
       });
 
       // Check response
       core.debug('Checking tag response ...')
       if (tagResponse.status !== 201) {
         core.setFailed(`Failed to create tag, tagResponse.status: ${tagResponse.status}`)
-      }
 
-      // Create ref (this is equivalent to git push, I think)
-      core.debug('Creating tag ref ...')
-      return client.git.createRef({
-        owner: ref.owner,
-        repo: ref.repo,
-        ref: 'refs/tags/' + nextVersion,
-        sha: tagResponse.data.sha,
-      }).then(() => {
-        return createPRCommentOnce(client, `Merged and tagged as \`${nextVersion}\`.`)
-      });
+      } else {
+        // Create ref (this is equivalent to git push, I think)
+        core.debug('Creating tag ref ...')
+        return client.git.createRef({
+          owner: ref.owner,
+          repo: ref.repo,
+          ref: 'refs/tags/' + nextVersion,
+          sha: tagResponse.data.sha,
+        }).then(() => {
+          return createPRCommentOnce(client, `Merged and tagged as \`${nextVersion}\`.`)
+        });
+      }
     }
     // otherwise update the title, and make a comment.
     return Promise.all([
